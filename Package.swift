3e5267aec55a69498739de0c5a23913111d376ae
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-07-30 21:06:55 -0400

    remove unused files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,13 +6,19 @@ import PackageDescription
 let package = Package(
     name: "RequestBuilder",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v17),
+        .macCatalyst(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RequestBuilder",
-            targets: ["RequestBuilder"]),
+            targets: ["RequestBuilder"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,9 +29,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RequestBuilder",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "RequestBuilderTests",
-            dependencies: ["RequestBuilder"]),
+            dependencies: ["RequestBuilder"]
+        ),
     ]
 )
